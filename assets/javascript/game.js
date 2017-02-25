f552da7e36ab4988e156275431f760c91bef16c4
Kelly Davis <kellydavis1974@gmail.com>
2017-02-25 02:52:59 -0500

    solved problem of repeat events on click listeners by isolating addition of events outside of looping areas of game; completed game round and finish logic and messaging; completed reset function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,134 +1,111 @@
-/* pseudo code
-------------------------
-Game flow
-1. User chooses character from character bank
-2. Character is moved from character bank to player character area
-3. User chooses opponent
-4. Opponent is moved to defender area
-5. User clicks Attack button
-6. Game round:
-	a) user HP goes down by defender's attack power
-	b) opponent HP goes down by user attack power
-	c) user attack power goes up by <?>
-7. Results calculated
-	a) Are user HP <= 0?
-		Game over message and restart option
-	b) Opponent HP <= 0?
-		User wins round
-			1. Is this last defender?
-				User wins game
-					Message and restart option
-			2. Not last defender?
-				Move on
-8. User chooses new opponent
-9. New opponent moves to Defender area
-10. Back to step 5.
-
-
-Variables
-----------------
-
-
-userChar will be one of the char objects assigned to it by user choice
-opponent will be one of the remaining char objects assigned to it by user choice
-*/
-
 $(document).ready(function(){
 
+// opponents is a clone of the character divs that will be removed during game play, for reset
+var opponents = jQuery.extend(true,{}, $(".character-box"));
+
+// our game data tracing object
 var gameData = {
 	chars: {
 		leia: {
 			bg: 'assets/images/leia-bg.jpg',
-			hp: 140,
-			ap: 6
+			hp: 120,
+			ap: 8
 			},
 		chewbacca: {
 			bg: 'assets/images/chewbacca-bg.jpg',
-			hp: 180,
-			ap: 8
+			hp: 125,
+			ap: 10
 			},
 		phasma: {
 			bg: 'assets/images/phasma-bg.jpg',
 			hp: 140,
-			ap: 10
+			ap: 15
 			},
 		kylo: {
 			bg: 'assets/images/kylo-bg.jpg',
-			hp: 160,
-			ap: 11
+			hp: 170,
+			ap: 18
 			}
 		},
-	defendersRemaining: 4,	
+	defendersRemaining: 3,
 	choice: 'user'
-	}
-
-	// fx is the sound effects player object
-	var fx = document.createElement('audio');
-
-	clicksOn();
-
-	function chooser(){
-		if (gameData.choice == 'user') {
-			userChar=this;
-			// set base power; we'll need this in the attack round
-			basePower=gameData.chars[this.id].ap;
-			$(userChar).off().css('cursor','auto');
-			$('#userCharacter').append("<h2>Your character</h2>").append(userChar);
-			$('#instructions').html('Choose an opponent');
-			
-			// reset background image to match user character
-			$('body').css('background-image','url('+gameData.chars[this.id].bg+')').css('color','#fff');
-			$('.character-box p').css('color','#000');
-			gameData.choice = 'opponent'
-		}
-		else if (gameData.choice == 'opponent') {
-			opponent=this;
-			// turn off all click listeners
-			$(".character-box").off().css('cursor','auto');
-			
-			// move choice of opponent to 'defender' area
-			$('#defender').append("<h2>Defender</h2>").append(opponent);
-			$('#instructions').html('Time to attack!');
-			$('#attackButton').fadeIn('slow').on("click",attack);
-		}
-	}
-
-	function clicksOn() {
-		$(".character-box").each(function(){
-		$(this).append('<p class="hp">Health: '+gameData.chars[this.id].hp+'</p>').on("click",chooser).css('color','#000')
-		});
 	};
-	
-	function attack() {
-		// choose random sound file and play it
-		var soundfile = Math.floor(Math.random()*5+1);
-		fx.setAttribute('src','assets/sounds/sound'+soundfile+'.mp3');
-		fx.play();
-		
-		var user = gameData.chars[userChar.id];
-		var opp = gameData.chars[opponent.id];
-		
-		// user attacks
-		opp.hp=opp.hp-user.ap;
-		
-		// user attack power goes up
-		user.ap=user.ap+basePower;
 
-		// is opponent defeated?
-		if (opp.hp <= 0) {
-			// defender count decrements
-			gameData.defendersRemaining--;
-			// no defenders left?
-			if (gameData.defendersRemaining==0)
-				gameOver('win')
-			else 
-				//round is over but not game
-				roundOver();
-			}
+// add click events to attack and reset buttons
+$("#attackButton").on("click",attack);
+$("#resetButton").on("click",reset);
+
+// fx is the audio player object, for sound fx
+var fx = document.createElement('audio');
+
+// add click listeners to character divs
+clicksOn();
+
+// make a clone of gameData to ease reset
+var resetData = jQuery.extend(true,{}, gameData);
+
+function chooser(){
+	if (gameData.choice == 'user') {
+		userChar=this;
+		// set base power; we'll need this in the attack round
+		basePower=gameData.chars[this.id].ap;
+		$(userChar).off().css('cursor','auto');
+		$('#userCharacter').append("<h2>Your character</h2>").append(userChar);
+		$('#instructions').html('Choose an opponent');
 		
-		else  
-			// just update opponent health points
-			$(opponent).children(".hp").html('Health: '+opp.hp);
+		// reset background image to match user character
+		$('body').css('background-image','url('+gameData.chars[this.id].bg+')').css('color','#fff');
+		$('.character-box p').css('color','#000');
+		gameData.choice = 'opponent'
+	}
+	else if (gameData.choice == 'opponent') {
+		opponent=this;
+		// turn off all click listeners
+		$(".character-box").off().css('cursor','auto');
+		
+		// move choice of opponent to 'defender' area
+		$('#defender').empty().append("<h2>Defender</h2>").append(opponent);
+		
+		// tell user what to do now
+		$('#instructions').html('Time to attack!');
+		
+		// provide an attack button
+		$('#attackButton').fadeIn("slow");
+	}
+}
+
+// <this> in clicksOn function is each character div in the html for the user to choose from
+// we append health pts from gameData object and an event listener that invokes the chooser function, and adjusts cursor style to pointer
+// we also change the font color because chooser changes the gamestage background
+function clicksOn() {
+	$(".character-box").off().each(function(){
+	$(this).append('<p class="hp">Health: '+gameData.chars[this.id].hp+'</p>').on("click",chooser).css('color','#000').css('cursor','pointer');
+	});
+};
+
+function attack() {
+	// kill any as-yet playing fx
+	soundOff();
+	// choose new random fx file and play it
+	var soundfile = Math.floor(Math.random()*5+1);
+	sound('assets/sounds/sound'+soundfile+'.mp3');
+	
+	var user = gameData.chars[userChar.id];
+	var opp = gameData.chars[opponent.id];
+	
+	// user attacks
+	opp.hp=opp.hp-user.ap;
+	
+	// user attack power goes up
+	user.ap=user.ap+basePower;
+
+	// is opponent defeated?
+	if (opp.hp <= 0) 
+		//round is over 
+		roundOver()		
+	else { 
+		// just update opponent health points
+		$(opponent).children(".hp").html('Health: '+opp.hp);
 
 		// opponent counter attacks
 		user.hp=user.hp-opp.ap;
@@ -137,26 +114,95 @@ var gameData = {
 			user.hp = 0;
 			// update health point display
 			$(userChar).children(".hp").html('Health: '+user.hp);
-			gameOver('lose')
+			gameOver(0)
 			}
 		else // just update user HP
 			$(userChar).children(".hp").html('Health: '+user.hp);
-		
-		// user attack power always rises by base power for next round
-		
-		
-		function roundOver(){
-			//remove defeated opponent from stage
-			$("#defender").empty();
-			
-			// hide attack button
-			$('#attackButton').fadeOut('slow');
-			// re-activate character divs
-			clicksOn();
-			// instruct player to choose new opponent
-			$('#instructions').html('Choose another opponent');
 		}
-		
-	}
+}
+	
+function roundOver(){
 
-	});  // end doc ready
+	//remove defeated opponent from stage
+	$("#defender").empty();
+	
+	// hide attack button
+	$('#attackButton').fadeOut("slow");
+	
+	// defender count decrements
+	gameData.defendersRemaining--;
+	
+	// any defenders left?
+	if (gameData.defendersRemaining <= 0)
+		gameOver(1)
+
+	else {
+		// re-activate character divs
+		clicksOn();
+	
+		// instruct player to choose new opponent
+		$('#instructions').html('Choose another opponent')
+	}
+}
+
+function gameOver(win) {
+	if (win) {
+		// cue the victory march; make it a timeout to give the attack sound effect time to play
+		sound('assets/sounds/victory.mp3');
+		$("#defender").empty();
+		$("#instructions").html('You are the victor! May the Force be with you!')
+	}
+	else {
+		// cue the dark side; make it a timeout to give the attack sound effect time to play
+		sound('assets/sounds/vader.mp3');
+		$("#userCharacter").empty();
+		$("#instructions").html('You have been defeated by the Dark Side!')
+	}
+	// hide attack button 
+	$("#attackButton").fadeOut("slow");
+	
+	// show reset button and add click function
+	$("#resetButton").fadeIn("slow");
+
+} // end gameOver function
+
+function reset() {
+
+	// kill any fx
+	soundOff();
+	
+	// set gameData and game message to initial values
+	gameData = resetData;
+	$("#instructions").html("Choose a character");
+
+	
+	//empty user and opponent character area
+	$("#userCharacter").empty();
+	$("#defender").empty();
+
+	// restore characters
+	$(opponents).each(function(){
+		$("#characters").append(this)
+		});
+
+	// restore click listeners on character divs
+	clicksOn();
+
+	// hide reset button
+	$("#resetButton").fadeOut("slow");
+
+	//reset body background
+	$("body").css("background-image","url(assets/images/background.jpg");
+
+}  // end click function on reset button
+
+function sound(file) {
+	fx.setAttribute('src',file);
+	fx.play();
+}
+
+function soundOff() {
+	fx.pause();
+}
+
+});  // end doc ready
